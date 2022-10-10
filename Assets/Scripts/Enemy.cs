dd0347d48a88dfd8cab2acc688b07e64d5341810
artsung410 <61309735+artsung410@users.noreply.github.com>
2022-10-11 00:10:07 +0900

    [Feat] 맵 제작, 타겟 감지 / 추적

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    private void Start()
+    {
+        target = Waypoints.points[0];
+    }
+
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWayPoint();
+        }
+    }
+
+    // 다음 way포인트 할당
+    void GetNextWayPoint()
+    {
+        if (wavepointIndex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        wavepointIndex++;
+        target = Waypoints.points[wavepointIndex];
+    }
+}
