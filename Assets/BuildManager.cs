638d9499116c54a25a426384d6d05a2dfc10ead7
artsung410 <zpzpskm@naver.com>
2022-10-11 12:22:22 +0900

    [Feat] camera 이동, Zoom In / Out

---
+++ Assets/BuildManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More than one BuildManager in scene!");
+            return;
+        }
+
+        instance = this;
+    }
+
+    public GameObject standardTurretPrefab;
+    private GameObject turretToBuild;
+
+    private void Start()
+    {
+        turretToBuild = standardTurretPrefab;
+    }
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+}
